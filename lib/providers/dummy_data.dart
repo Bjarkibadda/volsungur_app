b05184c266af4814a7e56ed227955bc237c47924
Bjarki Baldvinsson <bjarkibaldvinsson@Bjarkis-MacBook-Air.local>
2020-07-14 15:16:23 +0000

    fetching from database

--- lib/providers/dummy_data.dart
+++ lib/providers/dummy_data.dart
@@ -1,24 +1,48 @@
 import 'package:flutter/material.dart';
 import './practice_model.dart';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Practices with ChangeNotifier {
   List<Practice> _fixedTrainings = [
     Practice(
-        id: DateTime.now().toString(),
-        name: 'training1',
-        url:
-            "https://www.youtube.com/watch?v=bdneye4pzMw&list=RDbdneye4pzMw&start_radio=1",
-        isRequired: false),
-    Practice(
-        id: DateTime.now().toString(),
-        name: 'training2',
-        url:
-            "https://www.youtube.com/watch?v=OPf0YbXqDm0&list=RDbdneye4pzMw&index=2",
-        isRequired: false,),
+        //     id: DateTime.now().toString(),
+        //     name: 'training1',
+        //     url:
+        //         "https://www.youtube.com/watch?v=bdneye4pzMw&list=RDbdneye4pzMw&start_radio=1",
+        //     isRequired: false),
+        // Practice(
+        //   id: DateTime.now().toString(),
+        //   name: 'training2',
+        //   url:
+        //       "https://www.youtube.com/watch?v=OPf0YbXqDm0&list=RDbdneye4pzMw&index=2",
+        //   isRequired: false,
+        ),
   ];
 
   List<Practice> get items {
     return [..._fixedTrainings];
+  }
+
+  Future<void> fetchTrainings() async {
+    const url = 'https://volsungurapp.firebaseio.com/Practices.json';
+    final rsp = await http.get(url);
+    final _data = json.decode(rsp.body) as Map<String, dynamic>;
+    final List<Practice> loadedPractices = [];
+    _data.forEach(
+      (practiceId, practiceData) {
+        loadedPractices.add(
+          Practice(
+              id: practiceId,
+              name: practiceData['name'],
+              url: practiceData['url'],
+              isRequired: practiceData['isRequired'],
+              isFavorite: practiceData['isFavorite']),
+        );
+      },
+    );
+    _fixedTrainings = loadedPractices;
+    notifyListeners();
   }
 }
--- lib/screens/training_screen.dart
+++ lib/screens/training_screen.dart
@@ -7,9 +7,20 @@ import '../providers/dummy_data.dart';
 import 'package:provider/provider.dart';
 import '../widgets/practice_item.dart';
 
-class TrainingScreen extends StatelessWidget {
+class TrainingScreen extends StatefulWidget {
   static const routeName = '/training_screen';
+
   @override
+  _TrainingScreenState createState() => _TrainingScreenState();
+}
+
+class _TrainingScreenState extends State<TrainingScreen> {
+  @override
+  void didChangeDependencies() {
+    Provider.of<Practices>(context).fetchTrainings();
+    super.didChangeDependencies();
+  }
+
   Widget build(BuildContext context) {
     final train = Provider.of<Practices>(context);
     return Scaffold(
@@ -21,51 +32,42 @@ class TrainingScreen extends StatelessWidget {
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
-            child: Text('Æfingar vikunnar', style:TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            child: Text('Æfingar vikunnar',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
           ),
           Container(
-            height:400,
-            child: GridView.builder(
-              padding: const EdgeInsets.all(10.0),
-              itemCount: train.items.length,
-              itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
-                value: train.items[i],
-                child: PracticeItem(train.items[i].name),
-              ),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 3 / 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10),
-
-              //   Card(
-
-              //       color: Colors.pink,
-              //       child: PraciceItem(),),
-              //   Card(
-              //       margin: EdgeInsets.all(7),
-              //       color: Colors.pink,
-              //       child: Text(train.items[0].url.toString())),
-              //   Card(
-              //       margin: EdgeInsets.all(7),
-              //       color: Colors.pink,
-              //       child: Text(train.items[0].url.toString())),
-              //   Card(
-              //       margin: EdgeInsets.all(7),
-              //       color: Colors.pink,
-              //       child: Text(train.items[0].url.toString())),
-              //   Card(
-              //       margin: EdgeInsets.all(7),
-              //       color: Colors.pink,
-              //       child: Text(train.items[0].url
-              //           .toString())), // setja þetta í nýjan file - widget
-              // ],
-              // gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              //maxCrossAxisExtent: 200),
-            ),
+            height: 300,
+            child: PracticeGrid(train: train),
           ),
+          Divider(color: Colors.pink),
         ],
       ),
     ); //);
+  }
+}
+
+class PracticeGrid extends StatelessWidget {
+  const PracticeGrid({
+    Key key,
+    @required this.train,
+  }) : super(key: key);
+
+  final Practices train;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: train.items.length,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        value: train.items[i],
+        child: PracticeItem(train.items[i].name),
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10),
+    );
   }
 }
